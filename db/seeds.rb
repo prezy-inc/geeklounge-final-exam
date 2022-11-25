9121875e7a69b9e91d052117ad8f1ba603854f2d
isosea <isokazu.psy@gmail.com>
2022-11-25 08:07:20 +0000

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# メインのサンプルユーザーを1人作成する
+User.create!(name:  "Example User",
+             email: "study@geek-lounge.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name  = Faker::Name.name
+  email = "study-#{n+1}@geek-lounge.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
