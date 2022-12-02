51f99fb267c44871d27a893795e4b9ce74d455a0
isosea <isokazu.psy@gmail.com>
2022-12-02 06:32:46 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -16,4 +18,5 @@ Rails.application.routes.draw do
   resources :comments, only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
